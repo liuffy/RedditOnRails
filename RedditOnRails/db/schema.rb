6e3b60b627054d527529e479e59edca9ea111ac8
Sophia Liu <sliu3@wellesley.edu>
2016-10-19 15:22:13 -0700

    Comments

--- RedditOnRails/db/schema.rb
+++ RedditOnRails/db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019203300) do
+ActiveRecord::Schema.define(version: 20161019215945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "post_id",           null: false
+    t.integer  "user_id",           null: false
+    t.text     "content",           null: false
+    t.integer  "parent_comment_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "post_subs", force: :cascade do |t|
     t.datetime "created_at"
